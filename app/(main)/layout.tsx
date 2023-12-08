6d521065bcfc6bfd25b572d6ad28d690e7313c68
Udoddy <77205282+Udoddy@users.noreply.github.com>
2023-12-08 16:47:08 +0100

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,16 +6,16 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+    title: 'Project23',
+    description: 'The 2023 project.',
     robots: { index: false, follow: false },
     viewport: { initialScale: 1, width: 'device-width' },
     openGraph: {
         type: 'website',
-        title: 'PrimeReact SAKAI-REACT',
-        url: 'https://sakai.primereact.org/',
-        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
-        images: ['https://www.primefaces.org/static/social/sakai-react.png'],
+        title: 'Project23',
+        url: 'https://project23-seven.vercel.app/',
+        description: 'The 2023 project.',
+        images: [''],
         ttl: 604800
     },
     icons: {
